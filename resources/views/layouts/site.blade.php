cd5a6d23fcdd68165beb74d2bfb36f2f715bbc2c
Andressafatec <dede.ginevro2021@gmail.com>
2023-09-13 11:49:08 -0300

    não lembro o que alterei

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -29,7 +29,7 @@
   <meta name="theme-color" content="#ffffff">
 
   <link rel="stylesheet" href="{{ asset('build/assets/app-408f3d12.css')}}">
-  <link rel="stylesheet" href="{{ asset('build/assets/app-fb635d16.css')}}">
+  <link rel="stylesheet" href="{{ asset('build/assets/app-e3d6e151.css')}}">
 
   @yield('head')
 
@@ -164,7 +164,7 @@
 
    @include('layouts._modalSearch')
 
-  <script src="{{asset('build/assets/app-ea37bd58.js')}}"></script>
+  <script src="{{asset('build/assets/app-1bc24b34.js')}}"></script>
    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
